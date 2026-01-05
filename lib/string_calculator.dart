7384cc506cbd3b72a3a85e06d0ee3fde193b6bb6
kshanlvs <sbittu348@gmail.com>
2026-01-05 12:28:30 +0530

    feat: support newline as delimiter

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -2,12 +2,10 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    if (numbers.contains(',')) {
-      final values = numbers.split(',').map(int.parse);
-      return values.reduce((a, b) => a + b);
-    }
+    final delimiter = RegExp(',|\n');
+    final values = numbers.split(delimiter).map(int.parse);
 
-    return int.parse(numbers);
+    return values.reduce((a, b) => a + b);
   }
 }
 
