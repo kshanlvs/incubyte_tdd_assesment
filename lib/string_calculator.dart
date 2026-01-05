cb505ae4e2b98262140cd39c69629ffd5a9a76d9
kshanlvs <sbittu348@gmail.com>
2026-01-05 12:34:15 +0530

    feat: support custom delimiter

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -2,8 +2,19 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    final delimiter = RegExp(',|\n');
-    final values = numbers.split(delimiter).map(int.parse);
+    String delimiterPattern = ',|\n';
+    String numbersSection = numbers;
+
+    if (numbers.startsWith('//')) {
+      final parts = numbers.split('\n');
+      final customDelimiter = parts.first.substring(2);
+      delimiterPattern = customDelimiter;
+      numbersSection = parts.last;
+    }
+
+    final values = numbersSection
+        .split(RegExp(delimiterPattern))
+        .map(int.parse);
 
     return values.reduce((a, b) => a + b);
   }
