2b019dad30144dcfa4781f8a4ef226d86d98ca14
kshanlvs <sbittu348@gmail.com>
2026-01-05 12:39:36 +0530

    test: throw exception for negative numbers

--- lib/test/string_calculator_test.dart
+++ lib/test/string_calculator_test.dart
@@ -21,4 +21,35 @@ void main() {
 test('supports custom delimiter', () {
   expect(calculator.add('//;\n1;2'), 3);
 });
+
+test('throws exception for single negative number', () {
+  expect(
+    () => calculator.add('1,-2'),
+    throwsA(
+      predicate(
+        (e) =>
+            e is Exception &&
+            e.toString().contains(
+              'negative numbers not allowed -2',
+            ),
+      ),
+    ),
+  );
+});
+
+test('throws exception with all negative numbers', () {
+  expect(
+    () => calculator.add('1,-2,-3'),
+    throwsA(
+      predicate(
+        (e) =>
+            e is Exception &&
+            e.toString().contains(
+              'negative numbers not allowed -2,-3',
+            ),
+      ),
+    ),
+  );
+});
+
 }
