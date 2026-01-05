fcd959cbf029e651c77e98f75fb50e0be5cf7169
kshanlvs <sbittu348@gmail.com>
2026-01-05 12:23:14 +0530

    feat: support comma separated numbers

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,6 +1,13 @@
 class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
+
+    if (numbers.contains(',')) {
+      final values = numbers.split(',').map(int.parse);
+      return values.reduce((a, b) => a + b);
+    }
+
     return int.parse(numbers);
   }
 }
+
