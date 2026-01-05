7bfb4d8d56776934ed599d89dcabdba34298b276
kshanlvs <sbittu348@gmail.com>
2026-01-05 12:01:23 +0530

    test: add test for single number input

--- lib/test/string_calculator_test.dart
+++ lib/test/string_calculator_test.dart
@@ -2,8 +2,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:incubyte_tdd_assesment/string_calculator.dart';
 
 void main() {
+      final calculator = StringCalculator();
   test('returns 0 for empty string', () {
-    final calculator = StringCalculator();
+
     expect(calculator.add(''), 0);
+  });
+
+    test('returns number itself for single number', () {
+    expect(calculator.add('1'), 1);
   });
 }
